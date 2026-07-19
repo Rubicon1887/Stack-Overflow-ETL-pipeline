d1e295f41a89d6903751a7d823effdeffdd0f605
Atharva Barwe <abarwe@asu.edu>
2026-07-19 05:17:45 -0700

    tested saving json locally before AWS S3

--- primary/extract.py
+++ primary/extract.py
@@ -3,6 +3,8 @@ from datetime import datetime,time,timedelta,timezone
 from termcolor import colored
 from dotenv import load_dotenv
 import os
+from pathlib import Path
+import json
 
 load_dotenv()
 
@@ -25,3 +27,25 @@ def fetch_yesterdays_questions(tags): # I might not need start_date and end_date
     print(colored(f'Successfully fetched {len(qs['items'])} questions','green'))
 
     return qs
+
+def save_raw_questions(tags):
+    
+    data=fetch_yesterdays_questions(tags)
+
+    now=datetime.now()
+    path=Path(
+        r'C:\Users\athar\Documents\GitHub\personal project\Stack Overflow ETL pipeline\S3 data',
+        f'{now.year}',
+        f'{now.month:02d}',
+        f'{now.day:02d}'
+    )
+
+    path.mkdir(parents=True,exist_ok=True)
+    filepath=path/f'{tags}_questions.json'
+
+    with open(filepath,'w',encoding='utf-8') as f:
+        json.dump(data,f,indent=2)
+    print(f'Saved {now.date()} data to {filepath}')
+
+if __name__=='__main__':
+    save_raw_questions('python')
