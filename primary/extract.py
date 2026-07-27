a67f791d9f0db065a01cab591f741471e8181e7b
Atharva Barwe <abarwe@asu.edu>
2026-07-27 13:36:22 -0700

    make extract.py's methods reusable for both a daily job and backfill

--- primary/extract.py
+++ primary/extract.py
@@ -1,5 +1,5 @@
 from stackapi import StackAPI
-from datetime import datetime,time,timedelta,timezone
+from datetime import datetime,time,timedelta,timezone,date
 from termcolor import colored
 from dotenv import load_dotenv
 import os
@@ -12,32 +12,52 @@ load_dotenv()
 api_key=os.getenv('STACK_API_KEY')
 bucket_name=os.getenv('S3_BUCKET_NAME')
 
-def fetch_yesterdays_questions(tags):
-    
-    SITE=StackAPI('stackoverflow',key=api_key)
-    SITE.page_size=100
-    SITE.max_pages=15
+SITE=StackAPI('stackoverflow',key=api_key)
+SITE.page_size=100
+SITE.max_pages=15
 
-    today=datetime.now(timezone.utc).date()
-    yesterday=today-timedelta(days=1)
+client=boto3.client('s3')
 
-    fromdate=int(datetime.combine(yesterday,time.min,tzinfo=timezone.utc).timestamp())
-    todate=int(datetime.combine(today,time.min,tzinfo=timezone.utc).timestamp())
+def fetch_1days_questions(tags,day0): # I want the choice between day 0 and yesterday to be made BEFORE these 3 methods are called, inside the driver code.
 
-    print(colored(f'1 - Fetching Stack Overflow questions from {yesterday} tagged with {tags}','yellow'))
+    day1=day0+timedelta(days=1)
+
+    fromdate=int(datetime.combine(day0,time.min,tzinfo=timezone.utc).timestamp())
+    todate=int(datetime.combine(day1,time.min,tzinfo=timezone.utc).timestamp())
+
+    # if day1 is None and day0 is None: # yesterday's questions
+        
+    #     today=datetime.now(timezone.utc).date()
+    #     yesterday=today-timedelta(days=1)
+
+    #     fromdate=int(datetime.combine(yesterday,time.min,tzinfo=timezone.utc).timestamp())
+    #     todate=int(datetime.combine(today,time.min,tzinfo=timezone.utc).timestamp())
+
+    #     print(colored(f'1 - Fetching Stack Overflow questions from {yesterday} tagged with {tags}','yellow'))
+
+    # elif day1 is not None and day0 is None: # questions for a day between data dump and yesterday
+
+    #     fromdate=int(datetime.combine(day0,time.min,tzinfo=timezone.utc).timestamp())
+    #     todate=int(datetime.combine(day1,time.min,tzinfo=timezone.utc).timestamp())
+
+    #     print(colored(f'1 - Fetching Stack Overflow questions from {day0} tagged with {tags}','yellow'))
+
+    # else:
+    #     raise ValueError('Neither or both of day1 and day0 must be None')
+
+    print(colored(f'1 - Fetching Stack Overflow questions from {day0} tagged with {tags}','yellow'))
     qs=SITE.fetch('questions',fromdate=fromdate,todate=todate,tagged=tags)
     print(colored(f'2 - Successfully fetched {len(qs['items'])} questions','green'))
 
     return qs
 
-def save_raw_questions(qs,tags):
+def save_raw_questions(qs,tags,day0):
 
-    now=datetime.now()
     path=Path(
         r'C:\Users\athar\Documents\GitHub\personal project\Stack Overflow ETL pipeline\S3 data',
-        f'{now.year}',
-        f'{now.month:02d}',
-        f'{now.day:02d}'
+        f'{day0.year}',
+        f'{day0.month:02d}',
+        f'{day0.day:02d}'
     )
 
     path.mkdir(parents=True,exist_ok=True)
@@ -45,33 +65,31 @@ def save_raw_questions(qs,tags):
 
     with open(filepath,'w',encoding='utf-8') as f:
         json.dump(qs,f,indent=2)
-    print(colored(f'3 - Saved {now.date()} data to {filepath}','blue'))
+    print(colored(f'3 - Saved {day0} data to {filepath}','blue'))
 
     return filepath
 
-def upload_to_s3(filepath,tags):
-
-    client=boto3.client('s3')
-    now=datetime.now()
+def upload_to_s3(filepath,tags,day0):
 
     key=(
         f'raw/'
-        f'{now.year}/'
-        f'{now.month:02d}/'
-        f'{now.day:02d}/'
+        f'{day0.year}/'
+        f'{day0.month:02d}/'
+        f'{day0.day:02d}/'
         f'{tags}_questions.json'
     )
 
     client.upload_file(filepath,bucket_name,key)
-    print(colored(f'4 - Uploaded {now.date()} file to s3://{bucket_name}/{key}','magenta'))
+    print(colored(f'4 - Uploaded {day0} file to s3://{bucket_name}/{key}','magenta'))
 
 def fetch_save_upload():
 
     tags='python'
+    day0=datetime.now(timezone.utc).date() # or date(year,month,day)
 
-    qs=fetch_yesterdays_questions(tags)
-    filepath=save_raw_questions(qs,tags)
-    upload_to_s3(filepath,tags)
+    qs=fetch_1days_questions(tags,day0)
+    filepath=save_raw_questions(qs,tags,day0)
+    upload_to_s3(filepath,tags,day0)
 
 
 if __name__=='__main__':
