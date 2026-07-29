4a449d8559ae0b36ffae067def437e42d5e3a165
Atharva Barwe <abarwe@asu.edu>
2026-07-28 22:37:09 -0700

    moved StackAPI instantiation to __init__

--- primary/extract.py
+++ primary/extract.py
@@ -12,12 +12,6 @@ load_dotenv()
 api_key=os.getenv('STACK_API_KEY')
 bucket_name=os.getenv('S3_BUCKET_NAME')
 
-SITE=StackAPI('stackoverflow',key=api_key)
-SITE.page_size=100
-SITE.max_pages=15
-
-client=boto3.client('s3')
-
 class Extract:
 
     def __init__(self,day0,tag,backfill=False):
@@ -25,6 +19,12 @@ class Extract:
         self.day0=day0
         self.tag=tag
         self.backfill=backfill
+
+        self.SITE=StackAPI('stackoverflow',key=api_key)
+        self.SITE.page_size=100
+        self.SITE.max_pages=15
+
+        self.client=boto3.client('s3')
     
     def fetch_1days_questions(self):
 
@@ -34,7 +34,7 @@ class Extract:
         todate=int(datetime.combine(day1,time.min,tzinfo=timezone.utc).timestamp())
 
         if not self.backfill: print(colored(f'1 - Fetching Stack Overflow questions from {self.day0} tagged with {self.tag}','yellow'))
-        qs=SITE.fetch('questions',fromdate=fromdate,todate=todate,tagged=self.tag)
+        qs=self.SITE.fetch('questions',fromdate=fromdate,todate=todate,tagged=self.tag)
         if not self.backfill: print(colored(f'2 - Successfully fetched {len(qs['items'])} questions','green'))
 
         return qs
@@ -67,5 +67,5 @@ class Extract:
             f'{self.tag}_questions.json'
         )
 
-        client.upload_file(filepath,bucket_name,key)
+        self.client.upload_file(filepath,bucket_name,key)
         if not self.backfill: print(colored(f'4 - Uploaded {self.day0} file to s3://{bucket_name}/{key}','magenta')) # may wanna print tag somewhere
