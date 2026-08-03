9f3f427cbb0b5ffad950bf7c21cd6d7df46c6936
Atharva Barwe <abarwe@asu.edu>
2026-08-02 18:50:11 -0700

    docker-compose and postgres

--- scripts/backfill.py
+++ scripts/backfill.py
@@ -15,8 +15,8 @@ from primary.extract import Extract
 def fetch_save_upload():
 
     tags=['python','java','javascript','typescript','c#']
-    start_date=date(2021,10,11)
-    end_date=date(2021,12,31) # inclusive
+    start_date=date(2011,5,12)
+    end_date=date(2013,12,31) # inclusive
 
     extractor=Extract(backfill=True)
 
