f67cc9e32e1a6083950ee5e4d883e909f4504a2d
Atharva Barwe <abarwe@asu.edu>
2026-08-24 21:58:34 -0700

    adding a CTE in the intermediate model to extract year, month, day, dow

--- postgres_proj/models/intermediate/int_questions.sql
+++ postgres_proj/models/intermediate/int_questions.sql
@@ -1,16 +1,26 @@
-SELECT 
-    question_id,
-    programming_language,
-    tags,
-    owner_id,
-    owner_reputation,
-    owner_name,
-    is_answered,
-    view_count,
-    closed_unix_timestamp,
-    answer_count,
-    score,
-    creation_unix_timestamp,
-    question_date,
-    upload_timestamp
-FROM {{source('stackoverflow','questions')}}
+with questions as (
+    SELECT 
+        question_id,
+        programming_language,
+        tags,
+        owner_id,
+        owner_reputation,
+        owner_name,
+        is_answered,
+        view_count,
+        to_timestamp(closed_unix_timestamp) as closed_timestamp,
+        answer_count,
+        score,
+        to_timestamp(creation_unix_timestamp) as creation_timestamp,
+        to_timestamp(creation_unix_timestamp)::date as question_date,
+        upload_timestamp
+    FROM {{ref('stg_questions')}}
+)
+
+SELECT
+    *,
+    extract(year from creation_timestamp) as question_year,
+    extract(month from creation_timestamp) as question_month,
+    extract(day from creation_timestamp) as question_day,
+    extract(dow from creation_timestamp) as question_dow
+FROM questions
