15ff45891ca2fc8197ee7cdf54b60799e229cc89
Atharva Barwe <abarwe@asu.edu>
2026-08-27 02:48:04 -0700

    Update mar_questions.sql

--- postgres_proj/models/marts/mar_questions.sql
+++ postgres_proj/models/marts/mar_questions.sql
@@ -1,9 +1,16 @@
 {{ config(schema='marts') }}
 
-SELECT programming_language,question_year,COUNT(DISTINCT question_id)
-FROM {{ ref('int_questions') }}
-WHERE question_year!=2026
-GROUP BY programming_language,question_year
-ORDER BY programming_language,question_year ASC
+WITH year_counts AS (
+    SELECT programming_language,question_year,COUNT(DISTINCT question_id) AS question_count
+    FROM {{ ref('int_questions') }}
+    WHERE question_year!=2026
+    GROUP BY programming_language,question_year
+)
+
+SELECT
+    *,
+    SUM(question_count) OVER(PARTITION BY question_year) AS year_total,
+    ROUND(question_count/SUM(question_count) OVER(PARTITION BY question_year),2)*100 AS pct_share
+FROM year_counts
 
 -- TODO: Instead of raw counts per year, it's better to show the share of questions of each language (a %)
