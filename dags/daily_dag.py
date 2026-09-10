c80a56e480dd205ce92e2bab5005369f410974a0
Atharva Barwe <abarwe@asu.edu>
2026-09-10 02:15:33 -0700

    messing around with Dockerfile and the official Airflow docker compose

--- dags/daily_dag.py
+++ dags/daily_dag.py
@@ -1,7 +1,14 @@
+import pendulum
 from datetime import datetime
-# from airflow.sdk import dag,task
+from airflow.sdk import dag,task
 
 import sys
 sys.path.append('./')
 
 from primary.pipeline import StackOverflowPipeline
+
+@dag(
+    dag_id='daily_dag',
+    schedule='daily',
+    start_date=
+)
